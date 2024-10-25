57cb6dd3693c0f91cbfc8aab69469bb912423ccb
Swapneel.Patel <swapneelpatel@gmail.com>
2024-10-25 12:42:34 +0530

    code for wave from array

---
+++ src/main/java/org/example/ArraysWaveFromPrint.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ArraysWaveFromPrint {
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {1,4,3,2};
+
+        for(int i : waveFrom(arr))
+        {
+            System.out.print(i+ " ");
+        }
+    }
+
+
+    public static int [] waveFrom(int [] A)
+    {
+        for(int i =0;i<A.length;i+=2)
+        {
+            if ( i>0 && A[i] < A[i - 1]) {
+                swap(A, i, i - 1);
+            }
+            if ( i<A.length && A[i] < A[i + 1]) {
+                swap(A, i, i + 1);
+            }
+
+        }
+   return  A;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
--- src/main/java/org/example/Paildrom_using_Expand.java
+++ src/main/java/org/example/Paildrom_using_Expand.java
@@ -3,43 +3,31 @@ package org.example;
 public class Paildrom_using_Expand {
 
     public static void main(String[] args) {
-
         String s = "madam";
         System.out.println(expandAroundCenter(s));
-
     }
 
-
-
     public static boolean expandAroundCenter(String str) {
-       int length = str.length();
-  boolean ans = true;
+        int length = str.length();
+        boolean ans = true;
         int mid = length / 2;
-  int l,r;
-  if(length%2==0) {
-      l = mid;
-      r = mid-1;
-  }
-  else
-  {
-      l=mid;
-      r=mid;
-  }
-
-        while(l>=0 && r<str.length()) {
-            if (str.charAt(l) == str.charAt(r) ){
+        int l, r;
+        if (length % 2 == 0) {
+            l = mid;
+            r = mid - 1;
+        } else {
+            l = mid;
+            r = mid;
+        }
+        while (l >= 0 && r < str.length()) {
+            if (str.charAt(l) == str.charAt(r)) {
                 l--;
                 r++;
-
-
-            }
-            else {
+            } else {
                 ans = false;
                 break;
             }
-
         }
         return ans;
-
     }
 }
